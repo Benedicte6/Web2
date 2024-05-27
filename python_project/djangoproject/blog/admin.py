39bb74d8e5d169736a9e0c009678c0da36c12286
Ben <124366016+Benedicte6@users.noreply.github.com>
2024-05-27 11:32:50 +0200

    Final project
    End part project

--- python_project/djangoproject/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-# register each model with the admin site
-admin.site.register(BlogPost)
-admin.site.register(Event)
-admin.site.register(Article)
-admin.site.register(Agronome)
